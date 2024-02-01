45b8c41907e775f31c4aa0cb548caf74d6bf0748
Creray <creray.default@gmail.com>
2024-02-01 15:03:28 +0700

    Add color attribute

--- source/render/Renderer.cpp
+++ source/render/Renderer.cpp
@@ -12,25 +12,34 @@ namespace hellogl {
 
     const char* vertexSource = R"(
             #version 450 core
+
             layout (location = 0) in vec3 aPos;
+            layout (location = 1) in vec3 aColor;
+
+            out vec3 color;
+
             void main() {
                 gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+                color = aColor;
             }
     )";
 
     const char* fragmentSource = R"(
         #version 450 core
+
+        in vec3 color;
         out vec4 fragColor;
+
         void main() {
-                fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+                fragColor = vec4(color.rgb, 1.0);
         }
     )";
 
     const float Vertices[] {
-        -0.5, -0.5, 0.0,
-        -0.5, 0.5, 0.0,
-        0.5, 0.5, 0.0,
-        0.5, -0.5, 0.0
+        -0.5, -0.5, 0.0, 0.8, 0.2, 0.5,
+        -0.5, 0.5, 0.0, 0.3, 0.8, 0.2,
+        0.5, 0.5, 0.0, 0.1, 0.16, 0.9,
+        0.5, -0.5, 0.0, 0.2, 0.1, 0.5,
     };
 
     const int Indices[] {
@@ -63,8 +72,11 @@ namespace hellogl {
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
 
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), nullptr);
         glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+        glEnableVertexAttribArray(1);
 
         _defaultShader = make_unique<Shader>(vertexSource, fragmentSource);
     }
